ed35f6aa005e1a1fe4a8e8a60c3029e090dfb7a1
gowthamib <gowthamimona3@gmail.com>
2016-07-10 12:04:46 +0530

    finish

--- README.c
+++ README.c
@@ -1,37 +1,48 @@
 # code
-#include<stdio.h>
-#include<conio.h>
-void main()
-{
- int ar[100],i,j,n,tmp;
- printf(" Enter the size of the array \t");
- scanf("%d",&n);
- printf("Now enter the elements in the array \t");
- for(i=0;i<n;i++)
- {
-  scanf("%d",&ar[i]);
-}
-printf("\n Array is - ");
+int main(void) {
+int n,i,j=0,a[100],a1[100];
+scanf("%d",&n);
+for(i=0;i<n;i++)
+scanf("%d",&a[i]);
 for(i=0;i<n;i++)
 {
-printf("\t %d",ar[i]);
+int c=0;
+while(a[i])
+{
+int rem=a[i]%2;
+a[i]=a[i]/2;
+	c++;
+}
+a1[j++]=c;
 }
 for(i=0;i<n;i++)
 {
-for(j=0;j<n-i;j++)
+for(j=0;j<n;j++)
 {
-if(ar[j]<ar[j+1])
+if(a1[i]>a1[j])
 {
-tmp=ar[j+1];
-ar[j+1]=ar[j];
-ar[j]=tmp;
+int t=a1[i];
+a1[i]=a1[j];
+a1[j]=t;
+t=a[i];
+a[i]=a[j];
+a[j]=t;
 }
-}
-}
-printf("\n\n Array in the Descending order is - \n");
-for(i=1;i<=n;i++)
+else if(a1[i]==a1[j])
 {
- printf("\t %d",ar[i]);
+if(a[i]>a[j])
+{
+int t=a1[i];
+a1[i]=a1[j];
+a1[j]=t;
+t=a[i];
+a[i]=a[j];
+a[j]=t;
+}	
 }
- getch();
+}
+}
+for(i=0;i<n;i++)
+printf("%d ",a[i]);
+return 0;
 }
